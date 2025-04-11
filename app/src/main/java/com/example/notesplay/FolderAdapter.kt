5492f40829524e16375214b141998c0e43ac28be
Cizelle <rawatipshita07@gmail.com>
2025-04-11 23:50:50 +0530

    deleting folders

--- app/src/main/java/com/example/notesplay/FolderAdapter.kt
+++ app/src/main/java/com/example/notesplay/FolderAdapter.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import java.io.File
 
